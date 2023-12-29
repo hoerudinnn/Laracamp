5ff762f8e6f2eeff000b24dd0c922d13b55f328f
Hoerudin <hoerudinpermana30@gmail.com>
2023-12-29 21:36:00 +0700

    init commit table camps

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,12 +15,16 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('occupation')->nullable();
+            $table->boolean('is_admin')->default(false);
+            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
